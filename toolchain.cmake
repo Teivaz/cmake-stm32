691ce67687ef6ccc3bde0220114b1ff42b8f3684
Oleksandr Kuznietsov <github@teivaz.com>
2020-01-27 03:16:44 +0100

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -3,8 +3,9 @@ set(CMAKE_SYSTEM_NAME "Generic")
 
 # Compilers
 set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
-set(CMAKE_ASM_COMPILER "arm-none-eabi-gcc")
 
 # tells CMake not to try to link executables during its interal checks
 # things are not going to link properly without a linker script
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_OBJCOPY "arm-none-eabi-objcopy")
